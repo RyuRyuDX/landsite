9dc1be656402db4e5196f768981009549e87ef4d
Ryusuke Onose <gibson.red.gin.suke@gmail.com>
2021-10-19 19:28:46 +0900

    【ADD】SEO対策

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import '../styles/index.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
+import Seo from './components/Seo/Seo'
 
 export default function MyApp({ Component, pageProps }) {
   return ( 
     <>
+      <Seo />
       <Header />
       <Navbar />
         <Component {...pageProps} />
---
+++ pages/components/Seo/Seo.js
@@ -0,0 +1,12 @@
+import { NextSeo } from 'next-seo';
+
+export default function Seo() { 
+  return(
+  <>
+    <NextSeo
+      title="LAND"
+      description="茨城県日立市にある美容室、LAND HAIR CREATION LABです。常陸多賀駅から徒歩８分。くつろぎの空間と卓越した技術で心も髪もリフレッシュ♪あなただけの上質なサロンタイムを♪"
+    />
+  </>
+)
+}
